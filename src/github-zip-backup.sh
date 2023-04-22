55469ca384bd5d07667222c47a24b10394783cc0
Forrest Moulin <73726304+ffm5113@users.noreply.github.com>
2023-04-21 21:03:34 -0700

    Create github-zip-backup.sh

---
+++ src/github-zip-backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Replace the username with your GitHub username
+username="ffm5113"
+
+# Get a list of all repositories for the user
+repositories=$(curl -s httpsapi.github.comusers$usernamereposper_page=1000  grep -o 'git@[^]')
+
+# Download zip files for each repository
+for repo in $repositories; do
+    name=$(echo $repo  cut -d'' -f2  cut -d'.' -f1)
+    echo Downloading $name
+    curl -L -o $name.zip $repoarchivemaster.zip
+done
