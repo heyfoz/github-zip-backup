54764e7c3cd96cd5d8169028dd3923816de7ac11
Forrest Moulin <73726304+ffm5113@users.noreply.github.com>
2023-04-21 22:31:09 -0700

    Update github-zip-backup.sh

--- src/github-zip-backup.sh
+++ src/github-zip-backup.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-# Replace the username with your GitHub username
-username="insertGitHubUsernameHere"
+# GitHub API Documentation: 
+# https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#download-a-repository-archive-zip
 
-# Get a list of all repositories for the user
-repositories=$(curl -s httpsapi.github.comusers$usernamereposper_page=1000  grep -o 'git@[^]')
+# Set the username, authorization token, and destination directory
+USERNAME="insertGitHubUsername"
+TOKEN="insertGitHubAccessToken"
+DIRECTORY="C:\path\to\directory"
 
-# Download zip files for each repository
-for repo in $repositories; do
-    name=$(echo $repo  cut -d'' -f2  cut -d'.' -f1)
-    echo Downloading $name
-    curl -L -o $name.zip $repoarchivemaster.zip
+# Get list of repository names
+repos=$(curl -s -H "Authorization: token $TOKEN" "https://api.github.com/users/$USERNAME/repos?type=all&per_page=1000" | grep -Eo '"full_name": "[^"]+"' | awk '{print $2}' | tr -d \")
+
+# Loop through repositories
+for repo in $repos
+# Check for main | master default branches
+do
+  BRANCH=$(curl -s -H "Authorization: token $TOKEN" "https://api.github.com/repos/$repo/branches" | grep -E 'name.*(main|master)' | cut -d'"' -f4)
+  # If the repository has a master or main default branch, download it as a zip archive
+  if [[ ! -z "$BRANCH" ]]; then
+    echo "Downloading $repo as zip archive..."
+    curl -sL -H "Authorization: token $TOKEN" "https://api.github.com/repos/$repo/zipball/$BRANCH" -o "$DIRECTORY/${repo##*/}.zip"
+  fi
 done
