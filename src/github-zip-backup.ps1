7b3bda79a4bbe4d7b12576bc3a33eeb52fc3a063
Forrest Moulin <73726304+ffm5113@users.noreply.github.com>
2023-04-21 22:59:33 -0700

    Update github-zip-backup.ps1

--- src/github-zip-backup.ps1
+++ src/github-zip-backup.ps1
@@ -4,9 +4,9 @@
 # GitHub API Documentation: 
 # https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#download-a-repository-archive-zip
 
-$username = "insertGitHubUsername"
+$username = "ffm5113"
 
-# Get the user's repositories via github api rest method
+# Get the repositories for the user
 $uri = "https://api.github.com/users/$username/repos?per_page=1000"
 $repositories = Invoke-RestMethod -Uri $uri
 
@@ -15,20 +15,17 @@ foreach ($repo in $repositories) {
     $name = $repo.name
     $zipUri = $repo.archive_url -replace '{archive_format}{/ref}', 'zipball/master'
     $zipFile = "$name.zip"
-    Write-Host "Downloading $name..."
-    Invoke-WebRequest -Uri $zipUri -OutFile $zipFile
-    # Try to check for/download master branch. If not found, check for/download main branch
+    Write-Host "Downloading $name"
+
+    # check for master branch, if not found, check for main branch
     try {
         Invoke-WebRequest -Uri $zipUri -OutFile $zipFile -ErrorAction Stop
     } catch {
-        # Invoke-WebRequest returns 404 error in $_.Exception.Message if branch not found
         if ($_.Exception.Message -match '404') {
             $zipUri = $repo.archive_url -replace '{archive_format}{/ref}', 'zipball/main'
             Invoke-WebRequest -Uri $zipUri -OutFile $zipFile -ErrorAction Stop
-        } 
-        # Specify repo name in warning
-        else { 
-            Write-Warning "Error downloading $name: $($_.Exception.Message)"
+        } else {
+            Write-Warning "Error downloading $($name): $($_.Exception.Message)"
         }
     }
 }
