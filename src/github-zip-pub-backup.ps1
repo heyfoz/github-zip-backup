2582e9a3c275b2d639dbc54e8ec678ae5a4f213c
Forrest Moulin <73726304+ffm5113@users.noreply.github.com>
2023-04-22 01:11:03 -0700

    Update github-zip-pub-backup.ps1

--- src/github-zip-pub-backup.ps1
+++ src/github-zip-pub-backup.ps1
@@ -4,12 +4,15 @@
 # GitHub API Documentation: 
 # https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#download-a-repository-archive-zip
 
-# Example using just the username to access public repos (access token required for private repos)
 $username = "ffm5113"
 
 # Get the repositories for the user
 $uri = "https://api.github.com/users/$username/repos?per_page=1000"
 $repositories = Invoke-RestMethod -Uri $uri
+
+# Initialize int variables to keep track of the number of repositories downloaded
+$totalRepos = $repositories.Count
+$downloadRepos = 0
 
 # Download zip files for each repository
 foreach ($repo in $repositories) {
@@ -18,15 +21,21 @@ foreach ($repo in $repositories) {
     $zipFile = "$name.zip"
     Write-Host "Downloading $name"
 
-    # Check for/download master default branch, if not found, do same for main default branch
+    # check for master branch, if not found, check for main branch
     try {
         Invoke-WebRequest -Uri $zipUri -OutFile $zipFile -ErrorAction Stop
+	  $downloadRepos++
     } catch {
         if ($_.Exception.Message -match '404') {
             $zipUri = $repo.archive_url -replace '{archive_format}{/ref}', 'zipball/main'
             Invoke-WebRequest -Uri $zipUri -OutFile $zipFile -ErrorAction Stop
+		$downloadRepos++
         } else {
             Write-Warning "Error downloading $($name): $($_.Exception.Message)"
         }
     }
 }
+
+
+# Display the number of repositories downloaded
+Write-Host "$downloadRepos / $totalRepos repositories downloaded."
